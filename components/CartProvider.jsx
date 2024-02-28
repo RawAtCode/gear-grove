21ee844c9d7645d275b7bdb4fe5f061bc11392f3
rawatcode <rawatcode@gmail.com>
2024-02-28 23:33:44 +0530

    fixed some minor errors

--- components/CartProvider.jsx
+++ components/CartProvider.jsx
@@ -8,8 +8,8 @@ const CartProvider = ({children}) => {
     mode="payment" 
     cartMode="client-only" 
     stripe={process.env.NEXT_PUBLIC_STRIPE_KEY}
-    successUrl="https://geargrove.vercel.app/stripe/success" 
-    cancelUrl="https://geargrove.vercel.app/stripe/error"
+    successUrl="https://geargrove.vercel.app/Stripe/success" 
+    cancelUrl="https://geargrove.vercel.app/Stripe/error"
     language="en-US"
     currency="INR"
     billingAddressCollection={true}
