dc24386e78883238ba288dc2074b69135dfd84ae
rawatcode <rawatcode@gmail.com>
2024-02-28 23:32:06 +0530

    corrected Stripe to stripe directory

--- components/CartProvider.jsx
+++ components/CartProvider.jsx
@@ -8,7 +8,7 @@ const CartProvider = ({children}) => {
     mode="payment" 
     cartMode="client-only" 
     stripe={process.env.NEXT_PUBLIC_STRIPE_KEY}
-    successUrl="https://geargrove.vercel.app/stripe/success"
+    successUrl="https://geargrove.vercel.app/stripe/success" 
     cancelUrl="https://geargrove.vercel.app/stripe/error"
     language="en-US"
     currency="INR"
